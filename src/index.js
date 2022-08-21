81b45ee8834838f2df46e475acfa80c4f3be83bd
helan <helanmah@gmail.com>
2022-08-21 14:28:22 -0700

    added design layout

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@
 
 
 
-
-
-
-
 /*
 Things to add:
-- header + footer
 - sidebar with tabs: 'All', 'Today', 'This week', 'This month', 'Projects', 'add new task button'
 - Each task has: 'title', 'description', 'date created', 'optional due date', 'optional level of priority', checkbox, delete button, edit button
 - main area that shows all the tasks
